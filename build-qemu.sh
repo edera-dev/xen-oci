e631e56444c095128cc5cd426ba05a6b43d7e607
Alex Zenla <alex@edera.dev>
2024-11-07 04:01:34 -0500

    qemu-xen: introduce /opt/edera/sysroot and /opt/edera/qemu-xen

--- build-qemu.sh
+++ build-qemu.sh
@@ -4,7 +4,7 @@
 export CFLAGS="-O2 -Wall -Wno-error"
 echo "Building for: $target"
 env PKG_CONFIG_PATH="/usr/src/xen/tools/pkg-config:/usr/lib/pkg-config:/usr/share/pkg-config" \
-./configure --prefix=/usr --enable-xen --target-list=${target}-softmmu \
+./configure --prefix=/opt/edera/qemu-xen --enable-xen --target-list=${target}-softmmu \
   --extra-cflags="-I/usr/src/xen/tools/include -I/usr/src/xen/tools/libxc -I/usr/src/xen/tools/xenstore" \
   --extra-ldflags="-L/usr/src/xen/tools/libs/call -L/usr/src/xen/tools/libs/store -L/usr/src/xen/tools/libs/toollog -lxencall -lxenstore -lxentoollog"
 make -j$(nproc)
@@ -12,7 +12,7 @@ make install DESTDIR="/usr/src/qemu-xen/output"
 
 for PROG in elf2dmp qemu-edid qemu-ga qemu-img qemu-io qemu-nbd qemu-pr-helper qemu-storage-daemon qemu-system-${target} qemu-vmsr-helper; do
   echo "Setting interpreter for $PROG"
-  [ -x "/usr/src/qemu-xen/output/usr/bin/$PROG" ] && patchelf --set-interpreter /opt/edera/lib/ld-musl-$(apk --print-arch).so.1 --add-rpath /opt/edera/lib --add-rpath /opt/edera/usr/lib "/usr/src/qemu-xen/output/usr/bin/$PROG";
+  [ -x "/usr/src/qemu-xen/output/opt/edera/qemu-xen/bin/$PROG" ] && patchelf --set-interpreter /opt/edera/sysroot/lib/ld-musl-$(apk --print-arch).so.1 --add-rpath /opt/edera/sysroot/lib --add-rpath /opt/edera/sysroot/usr/lib "/usr/src/qemu-xen/output/opt/edera/qemu-xen/bin/$PROG";
 done
 
 echo "Build complete"
